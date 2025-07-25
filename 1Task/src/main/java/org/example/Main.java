fd0e57ab52067e0df505b331e24876386dafac33
Bartosz Janusz <bartosz.janusz@jetbrains.com>
2025-07-25 14:56:41 +0200

    branch8

--- 1Task/src/main/java/org/example/Main.java
+++ 1Task/src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -13,6 +15,8 @@ public class Main {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
-//a
-//b
-//c
+        }
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+    }
+}
