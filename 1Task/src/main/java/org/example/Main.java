ce4a1d995317ab6dbec39c190d3c7260edfcf248
Bartosz Janusz <bartosz.janusz@jetbrains.com>
2025-08-01 13:18:05 +0200

    Added add method and also added functionality to Main. Now it adds numbers from 0 to 9 to itself and prints them.

--- 1Task/src/main/java/org/example/Main.java
+++ 1Task/src/main/java/org/example/Main.java
@@ -14,6 +14,12 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+        for(int i = 0; i < 10; i++){
+            System.out.println(add(i, i));
+        }
+    }
+    private static int add(int x, int y){
+        return x + y;
     }
 }
 //x
